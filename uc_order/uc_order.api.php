42e680b013cfafe3db5e372951b0a1cebcb73e35
Dave Long <dave@longwaveconsulting.com>
2011-02-11 18:20:31 +0000

    #897342 by tunic: Add 'presave' op to hook_order().

--- uc_order/uc_order.api.php
+++ uc_order/uc_order.api.php
@@ -151,6 +151,9 @@ function hook_uc_line_item_data_alter(&$items) {
  *   This is variable and is based on the value of $op:
  *   - new: Called when an order is created. $order is a reference to the new
  *       order object, so modules may add to or modify the order at creation.
+ *   - presave: Before an order object is saved, the hook gets invoked with this
+ *       op to let other modules alter order data before it is written to the
+ *       database. $order is a reference to the order object.
  *   - save: When an order object is being saved, the hook gets invoked with this
  *       op to let other modules do any necessary saving. $order is a reference to
  *       the order object.
