b8c3d8cbb51d852817e8c3b60d5607e4fe5e240d
Dave Long <dave@longwaveconsulting.com>
2011-07-28 13:40:24 +0100

    Use if/endif and foreach/endforeach in invoice templates to improve readability.

--- uc_order/templates/uc-order--admin.tpl.php
+++ uc_order/templates/uc-order--admin.tpl.php
@@ -15,16 +15,16 @@
 
 <p>
 <?php echo t('Products:'); ?><br />
-<?php foreach ($products as $product) { ?>
+<?php foreach ($products as $product): ?>
 - <?php echo $product->qty; ?> x <?php echo $product->title . ' - ' . uc_currency_format($product->price * $product->qty); ?><br />
 &nbsp;&nbsp;<?php echo t('SKU: ') . $product->model; ?><br />
-    <?php if (isset($product->data['attributes']) && is_array($product->data['attributes']) && count($product->data['attributes']) > 0) { ?>
+    <?php if (isset($product->data['attributes']) && is_array($product->data['attributes']) && count($product->data['attributes']) > 0): ?>
     <?php foreach ($product->data['attributes'] as $attribute => $option) {
       echo '&nbsp;&nbsp;' . t('@attribute: @options', array('@attribute' => $attribute, '@options' => implode(', ', (array)$option))) . '<br />';
     } ?>
-    <?php } ?>
+    <?php endif; ?>
 <br />
-<?php } ?>
+<?php endforeach; ?>
 </p>
 
 <p>
--- uc_order/templates/uc-order--customer.tpl.php
+++ uc_order/templates/uc-order--customer.tpl.php
@@ -10,7 +10,7 @@
   <tr>
     <td>
       <table width="100%" border="0" cellspacing="0" cellpadding="5" align="center" bgcolor="#FFFFFF" style="font-family: verdana, arial, helvetica; font-size: small;">
-        <?php if ($business_header) { ?>
+        <?php if ($business_header): ?>
         <tr valign="top">
           <td>
             <table width="100%" style="font-family: verdana, arial, helvetica; font-size: small;">
@@ -31,24 +31,24 @@
             </table>
           </td>
         </tr>
-        <?php } ?>
+        <?php endif; ?>
 
         <tr valign="top">
           <td>
 
-            <?php if ($thank_you_message) { ?>
+            <?php if ($thank_you_message): ?>
             <p><b><?php echo t('Thanks for your order, !order_first_name!', array('!order_first_name' => $order_first_name)); ?></b></p>
 
-            <?php if (isset($_SESSION['new_user'])) { ?>
+            <?php if (isset($_SESSION['new_user'])): ?>
             <p><b><?php echo t('An account has been created for you with the following details:'); ?></b></p>
             <p><b><?php echo t('Username:'); ?></b> <?php echo $order_new_username; ?><br />
             <b><?php echo t('Password:'); ?></b> <?php echo $order_new_password; ?></p>
-            <?php } ?>
+            <?php endif; ?>
 
             <p><b><?php echo t('Want to manage your order online?'); ?></b><br />
             <?php echo t('If you need to check the status of your order, please visit our home page at !store_link and click on "My account" in the menu or login with the following link:', array('!store_link' => $store_link)); ?>
             <br /><br /><?php echo $site_login_link; ?></p>
-            <?php } ?>
+            <?php endif; ?>
 
             <table cellpadding="4" cellspacing="0" border="0" width="100%" style="font-family: verdana, arial, helvetica; font-size: small;">
               <tr>
@@ -76,7 +76,7 @@
                         <b><?php echo t('Billing Phone:'); ?></b><br />
                         <?php echo $order_billing_phone; ?><br />
                       </td>
-                      <?php if (uc_order_is_shippable($order)) { ?>
+                      <?php if (uc_order_is_shippable($order)): ?>
                       <td valign="top" width="50%">
                         <b><?php echo t('Shipping Address:'); ?></b><br />
                         <?php echo $order_shipping_address; ?><br />
@@ -84,7 +84,7 @@
                         <b><?php echo t('Shipping Phone:'); ?></b><br />
                         <?php echo $order_shipping_phone; ?><br />
                       </td>
-                      <?php } ?>
+                      <?php endif; ?>
                     </tr>
                   </table>
 
@@ -113,13 +113,13 @@
                 </td>
               </tr>
 
-              <?php if (uc_order_is_shippable($order)) { ?>
+              <?php if (uc_order_is_shippable($order)): ?>
               <tr>
                 <td colspan="2" bgcolor="#EEEEEE">
                   <font color="#CC6600"><b><?php echo t('Shipping Details:'); ?></b></font>
                 </td>
               </tr>
-              <?php } ?>
+              <?php endif; ?>
 
               <tr>
                 <td colspan="2">
@@ -143,7 +143,7 @@
                       </td>
                     </tr>
 
-                    <?php if ($shipping_method && uc_order_is_shippable($order)) { ?>
+                    <?php if ($shipping_method && uc_order_is_shippable($order)): ?>
                     <tr>
                       <td nowrap="nowrap">
                         <b><?php echo t('Shipping Method:'); ?></b>
@@ -152,7 +152,7 @@
                         <?php echo $order_shipping_method; ?>
                       </td>
                     </tr>
-                    <?php } ?>
+                    <?php endif; ?>
 
                     <tr>
                       <td nowrap="nowrap">
@@ -164,7 +164,7 @@
                     </tr>
 
                     <?php
-                    foreach ($line_items as $item) {
+                    foreach ($line_items as $item):
                       if ($item['line_item_id'] == 'subtotal' || $item['line_item_id'] == 'total') {
                         continue;
                       } ?>
@@ -178,7 +178,7 @@
                       </td>
                     </tr>
 
-                    <?php } ?>
+                    <?php endforeach; ?>
 
                     <tr>
                       <td>&nbsp;</td>
@@ -200,32 +200,28 @@
 
                         <table width="100%" style="font-family: verdana, arial, helvetica; font-size: small;">
 
-                          <?php
-                          if (is_array($order->products)) {
-                            foreach ($order->products as $product) {
-                          ?>
+                          <?php foreach ($order->products as $product): ?>
                           <tr>
                             <td valign="top" nowrap="nowrap">
                               <b><?php echo $product->qty; ?> x </b>
                             </td>
                             <td width="98%">
                               <b><?php echo $product->title . ' - ' . uc_currency_format($product->price * $product->qty); ?></b>
-                              <?php if ($product->qty > 1) {
+                              <?php if ($product->qty > 1):
                                 echo t('(!price each)', array('!price' => uc_currency_format($product->price)));
-                              } ?>
+                              endif; ?>
                               <br />
                               <?php echo t('SKU: ') . $product->model; ?><br />
-                              <?php if (isset($product->data['attributes']) && is_array($product->data['attributes']) && count($product->data['attributes']) > 0) { ?>
+                              <?php if (isset($product->data['attributes']) && is_array($product->data['attributes']) && count($product->data['attributes']) > 0): ?>
                               <?php
                                 foreach ($product->data['attributes'] as $attribute => $option) {
                                   echo '<li>' . t('@attribute: @options', array('@attribute' => $attribute, '@options' => implode(', ', (array)$option))) . '</li>';
                                 } ?>
-                              <?php } ?>
+                              <?php endif; ?>
                               <br />
                             </td>
                           </tr>
-                          <?php }
-                              }?>
+                          <?php endforeach; ?>
                         </table>
 
                       </td>
@@ -235,29 +231,29 @@
                 </td>
               </tr>
 
-              <?php if ($help_text || $email_text || $store_footer) { ?>
+              <?php if ($help_text || $email_text || $store_footer): ?>
               <tr>
                 <td colspan="2">
                   <hr noshade="noshade" size="1" /><br />
 
-                  <?php if ($help_text) { ?>
+                  <?php if ($help_text): ?>
                   <p><b><?php echo t('Where can I get help with reviewing my order?'); ?></b><br />
                   <?php echo t('To learn more about managing your orders on !store_link, please visit our <a href="!store_help_url">help page</a>.', array('!store_link' => $store_link, '!store_help_url' => $store_help_url)); ?>
                   <br /></p>
-                  <?php } ?>
+                  <?php endif; ?>
 
-                  <?php if ($email_text) { ?>
+                  <?php if ($email_text): ?>
                   <p><?php echo t('Please note: This e-mail message is an automated notification. Please do not reply to this message.'); ?></p>
 
                   <p><?php echo t('Thanks again for shopping with us.'); ?></p>
-                  <?php } ?>
+                  <?php endif; ?>
 
-                  <?php if ($store_footer) { ?>
+                  <?php if ($store_footer): ?>
                   <p><b><?php echo $store_link; ?></b><br /><b><?php echo $site_slogan; ?></b></p>
-                  <?php } ?>
+                  <?php endif; ?>
                 </td>
               </tr>
-              <?php } ?>
+              <?php endif; ?>
 
             </table>
           </td>
