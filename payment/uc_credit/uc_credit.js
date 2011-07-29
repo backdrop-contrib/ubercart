19b1e4c1d95bfce3395f201f310877e918a9c548
Dave Long <dave@longwaveconsulting.com>
2011-07-29 11:27:16 +0100

    Clean up credit card admin JavaScript.

--- payment/uc_credit/uc_credit.js
+++ payment/uc_credit/uc_credit.js
@@ -3,20 +3,16 @@
  * Utility functions for displaying credit card info on the order view screen.
  */
 
-/**
- * Initializes state.
- */
-jQuery(document).ready(
-  function () {
-    jQuery('#cc_details_title').show(0);
-    jQuery('#cc_details').hide(0);
-  }
-);
+Drupal.behaviors.ucCreditAdmin = {
+  attach: function(context, settings) {
+    jQuery('#cc_details_title').show();
+    jQuery('#cc_details').hide();
 
-/**
- * Toggles credit card details.
- */
-function toggle_card_details() {
-  jQuery('#cc_details').toggle();
-  jQuery('#cc_details_title').toggle();
+    jQuery('#cc_details_title', context).click(
+      function() {
+        jQuery('#cc_details_title').hide();
+        jQuery('#cc_details').show();
+      }
+    );
+  }
 }
