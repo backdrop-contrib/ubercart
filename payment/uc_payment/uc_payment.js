a52d63e354b5880ee60b64806e0f3f6404709c9a
Dave Long <dave@longwaveconsulting.com>
2011-09-06 18:45:07 +0100

    Remove useless checkbox on receive check form.

--- payment/uc_payment/uc_payment.js
+++ payment/uc_payment/uc_payment.js
@@ -9,17 +9,3 @@
 function cvv_info_popup() {
   var popup = window.open(Drupal.settings.ucURL.creditCardCVVInfo, 'CVV_Info', 'toolbar=0,scrollbars=1,location=0,statusbar=0,menubar=0,resizable=1,width=480,height=460,left=282,top=122');
 }
-
-/**
- * Toggles the payment fields on and off on the receive check form.
- */
-function receive_check_toggle(checked) {
-  if (!checked) {
-    jQuery('#edit-amount').removeAttr('disabled').val('');
-    jQuery('#edit-comment').removeAttr('disabled').val('');
-  }
-  else {
-    jQuery('#edit-amount').attr('disabled', 'true').val('-');
-    jQuery('#edit-comment').attr('disabled', 'true').val('-');
-  }
-}
