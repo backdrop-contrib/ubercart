7bd4a09d6dd9d6dfbae11d43a613a6b765940829
Tim Rohaly <tr@202830.no-reply.drupal.org>
2012-09-06 23:51:39 -0700

    Issue #1772854 by DanZ: Fix function prototype in shipping documentation. Add function comments.

--- shipping/uc_shipping/uc_shipping.api.php
+++ shipping/uc_shipping/uc_shipping.api.php
@@ -11,7 +11,16 @@
  */
 
 /**
- * Handles additional data for shipments.
+ * Handles additional data and activity for shipments.
+ *
+ * Adds additional activity after shipment objects are loaded from,
+ * saved to, or deleted from the database.  This is useful for shipment
+ * modules that store method-specific shipment data in separate tables that
+ * need to be kept in sync with the uc_shipments table.
+ *
+ * The members of the shipment object are the fields in the corresponding
+ * record of the uc_shipments table, plus $shipment->packages, an array
+ * of package objects as returned by uc_shipping_package_load().
  *
  * @param $op
  *   The action being taken on the shipment. One of the following values:
@@ -19,14 +28,17 @@
  *   - save: Changes to the shipment have been written.
  *   - delete: The shipment has been deleted and the packages are available
  *     for reshipment.
- * @param &$shipment
+ * @param $shipment
  *   The shipment object.
  *
  * @return
- *   Only given when $op is "load" . An array of extra data to be added to the
- *   shipment object.
+ *   Only given when $op is "load". An associative array of extra data to
+ *   be added to the shipment object. Each key/value element of the array
+ *   becomes a separate member of the shipment object. Elements of the array
+ *   with the same key as members of the shipment object replace those members
+ *   of the shipment object.
  */
-function hook_uc_shipment($op, &$shipment) {
+function hook_uc_shipment($op, $shipment) {
   switch ($op) {
     case 'save':
       $google_order_number = uc_google_checkout_get_google_number($shipment->order_id);
