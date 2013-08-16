f3fbbebd89a50d7006c82b1a2dafe8ee0820afe3
Dave Long <dave@longwaveconsulting.com>
2013-08-16 22:44:16 +0100

    Issue #2064723: Fixed favicon link in printable order invoice template.

--- uc_order/uc_order-invoice-page.tpl.php
+++ uc_order/uc_order-invoice-page.tpl.php
@@ -12,6 +12,7 @@
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="<?php print $language->language ?>" lang="<?php print $language->language ?>" dir="<?php print $language->dir ?>">
 
 <head>
+  <?php print $head; ?>
   <title><?php print $head_title; ?></title>
   <style type="text/css">
     .buttons {
