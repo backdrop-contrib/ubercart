f318e1db220fbc1d4a8d970fc76de451d6eb9593
Robert J. Lang <robert@langorigami.com>
2023-01-01 19:38:24 -0800

    Fixes and tweaks.

--- shipping/uc_quote/js/uc_quote.js
+++ shipping/uc_quote/js/uc_quote.js
@@ -17,8 +17,17 @@ Backdrop.behaviors.ucQuoteFieldsetSummaries = {
         vals.push(Backdrop.t('Not shippable'));
       }
 
-      var shipping_type = $('#edit-shipping-type option[selected]').text();
+      var shipping_type = $('#edit-shipping-type option:selected').text();
       vals.push(Backdrop.t('Default shipping type') + ': ' + Backdrop.checkPlain(shipping_type));
+
+      var street_address = $('#edit-shipping-address-street1').val();
+      if (street_address) {
+        var address_type = Backdrop.t('custom');
+      }
+      else {
+        var address_type = Backdrop.t('default');
+      }
+      vals.push(Backdrop.t('Pickup address') + ': ' + address_type);
 
       return vals.join('<br>');
     });
