25a4e6f8f57859fae5ea6c545087dc5e10e2f195
Robert J. Lang <robert@langorigami.com>
2023-01-03 18:31:21 -0800

    Make shipping details condition on whether shippable is selected.

--- shipping/uc_quote/js/uc_quote.js
+++ shipping/uc_quote/js/uc_quote.js
@@ -12,22 +12,22 @@ Backdrop.behaviors.ucQuoteFieldsetSummaries = {
 
       if ($('#edit-shippable', context).is(':checked')) {
         vals.push(Backdrop.t('Shippable'));
+
+        var shipping_type = $('#edit-shipping-type option:selected').text();
+        vals.push(Backdrop.t('Default shipping type') + ': ' + Backdrop.checkPlain(shipping_type));
+
+        var street_address = $('#edit-shipping-address-street1').val();
+        if (street_address) {
+          var address_type = Backdrop.t('custom');
+        }
+        else {
+          var address_type = Backdrop.t('default');
+        }
+        vals.push(Backdrop.t('Pickup address') + ': ' + address_type);
       }
       else {
         vals.push(Backdrop.t('Not shippable'));
       }
-
-      var shipping_type = $('#edit-shipping-type option:selected').text();
-      vals.push(Backdrop.t('Default shipping type') + ': ' + Backdrop.checkPlain(shipping_type));
-
-      var street_address = $('#edit-shipping-address-street1').val();
-      if (street_address) {
-        var address_type = Backdrop.t('custom');
-      }
-      else {
-        var address_type = Backdrop.t('default');
-      }
-      vals.push(Backdrop.t('Pickup address') + ': ' + address_type);
 
       return vals.join('<br>');
     });
